f42a5345170fcfc6906135c75247cefdce778049
Huzaifa-A750 <huzaifaafzal750@gmail.com>
2024-01-07 02:29:27 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          This is Tailwind Integrated with React
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;
